3959fe45992a7293efd2d01fadbf82b9d8dd580e
Floris Koek <florkoek@gmail.com>
2020-09-28 12:58:48 +0200

    More advanced animations for trial

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,11 @@
-gsap.to(".item", {duration: 2, x: 300});
-TweenLite.to(".item", 2, {rotation:360, transformOrigin:"right 50%"});
+gsap.to(".item", {
+    duration: 2,
+    x: 300,
+    rotation:360,
+    transformOrigin:"right 50%",
+    repeat: 3,
+    repeatDelay: 1,
+    yoyo: true,
+    ease: Power2.easeIn
+});
+// TweenLite.to(".item", 2, {rotation:360, transformOrigin:"right 50%"});
