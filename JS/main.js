8e4678a51ce28711d76e2214d13d373912fa7443
Floris Koek <florkoek@gmail.com>
2020-10-09 19:12:25 +0200

    Last settings where succesfull. Now on to another try for optimalisation

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-//Navigatie smoothscroll functie
+//Navigation smoothscroll
 $(document).ready(function(){
     $( ".nav-item > a" ).click(function( event ) {
         event.preventDefault();
@@ -22,64 +22,122 @@ gsap.to(".item", {
 });
 
 // ==========================
-// Timeline which handles the first fase of the animation
-// fo stand for fase one
+// initialisation and start of fase one
 // ==========================
 
-
-// Sets the settings for the timeline
 var f1 = gsap.timeline();
-f1.set(".bottleOne, .bottleTwo, .bottleThree", {transformOrigin: "50% 50%", rotation: -25});
+f1.set(".bottleOne, .bottleTwo, .bottleThree", {transformOrigin: "50% 50%", rotation: -35});
 
-// moves in the first bottle
-f1.from(".bottleOne", {
-    duration: 1.5,
-    opacity: 1,
+// moves in the first bottle and moves it to the position as intented. This is basicly just a glorified float for all purposes. Also rotates it to 0
+f1.fromTo(".bottleOne", {
+    duration: 2.5,
     x: -800
-    },0);
-
-//sets the first bottle up right
-f1.to(".bottleOne", {
+        },
+        {
     duration: 1.5,
+    x: 150,
     rotation: 0,
     ease: "back"
-},0.25);
+    },0);
 
-// moves in the second bottle
-f1.from(".bottleTwo", {
-    duration: 1.5,
-    opacity: 1,
+// moves in the second bottle and rotates it to 0
+f1.fromTo(".bottleTwo", {
+    duration: 2.5,
     x: -500
-    },0);
-
-//sets the second bottle up right
-f1.to(".bottleTwo", {
-    duration: 1.6,
-    rotation: 0,
-    ease: "back"
-},0.30);
-
-// moves in the thirt bottle
-f1.from(".bottleThree", {
+        },
+        {
     duration: 1.5,
-    opacity: 1,
-    x: -300
-    },0);
-
-//sets the third bottle up right
-f1.to(".bottleThree", {
-    duration: 1.7,
+    x: 150,
     rotation: 0,
     ease: "back"
-},0.30);
+    },0);
+
+// moves in the thirt bottle and rotates it to 0
+f1.fromTo(".bottleThree", {
+    duration: 2.5,
+    x: -300
+        },
+        {
+    duration: 1.5,
+    x: 150,
+    rotation: 0,
+    ease: "back"
+    },0);
 
 // ==========================
 // initialisation and start of fase two
 // ==========================
 
+//Setting the value where the bottles need to end up and making it invisible before the animation
 var f2 = gsap.timeline();
+f2
+.set(".bottleZeroOne", {x: -207, opacity: 0})
+.set(".bottleZeroTwo", {x: -185, opacity: 0})
+.set(".bottleZeroThree", {x: -165, opacity: 0});
 
-f2.set(".bottleZeroOne", {scale:0});
+// ******************************************
+//TODO: 
+//Start timeline after end of previous one
+// ******************************************
+
+// ******************************************
+//TODO: 
+//Have the initial bottles disapear
+// ******************************************
+
+// moves in the first zeroBottle and makes it visible
+f2
+.fromTo(".bottleZeroOne", {
+    duration: 2,
+    y: -1000,
+        },
+        {
+    ease: Power4.easeOut,
+    y: 0,
+    duration: 2.5
+    },0)
+
+.to(".bottleZeroOne", {
+    opacity: 1,
+    duration: 0.1
+},0)
+;
+
+// moves in the second zeroBottle and makes it visible
+f2
+.fromTo(".bottleZeroTwo", {
+    duration: 2,
+    y: -1000,
+        },
+        {
+    ease: Power4.easeOut,
+    y: 0,
+    duration: 2.5
+    },0.2)
+
+.to(".bottleZeroTwo", {
+        opacity: 1,
+        duration: 0.1
+    },0)
+;
+
+
+// moves in the thirth zeroBottle and makes it visible
+f2
+.fromTo(".bottleZeroThree", {
+    duration: 2,
+    y: -1000,
+        },
+        {
+    ease: Power4.easeOut,
+    y: 0,
+    duration: 2.5
+    },0.3)
+
+.to(".bottleZeroThree", {
+        opacity: 1,
+        duration: 0.1
+},0);
 
 // ==========================
 // initialisation and start of fase three
@@ -96,7 +154,7 @@ f3.set(".Heineken", {rotation: 20});
 //Move in the 0% and net zo lekker from the right with slight stagger
 f3.from(".Percentage, .Subheader", {
     x:400,
-    stagger: 0.1
+    stagger: 0.5
 },2);
 
 // Animating the heineken logo in
