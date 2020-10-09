3af8d0417a87a673fc09dbf298385b087cb719b2
Floris Koek <florkoek@gmail.com>
2020-10-09 17:35:09 +0200

    Added the first zero-bottle. Commiting in case shit goes sideways

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,10 @@
-// ==========================
-// Dag Hobie!
-// Het betreft de onderstaande code.
-// Ik snap niet zo goed waarom dit het niet doet, maar ik heb sowiezo nog wat moeite met het begrijpen van variables
-// Zou je kunnen toelichten wat hier het probleem is en ik anders moet doen?
-// Het liefste zodat ik in de toekomst snap wat ik dan moet doen :)
-// ==========================
-
-
+//Navigatie smoothscroll functie
 $(document).ready(function(){
     $( ".nav-item > a" ).click(function( event ) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
 });
-
-
-
-// ==========================
-// Tot hier
-// ==========================
-
-
 
 // ==========================
 // First GSAP animation
@@ -42,55 +26,87 @@ gsap.to(".item", {
 // fo stand for fase one
 // ==========================
 
+
 // Sets the settings for the timeline
-var fo = gsap.timeline();
-fo.set(".bottleOne, .bottleTwo, .bottleThree", {transformOrigin: "50% 50%", rotation: -25});
+var f1 = gsap.timeline();
+f1.set(".bottleOne, .bottleTwo, .bottleThree", {transformOrigin: "50% 50%", rotation: -25});
 
-// Animating the heineken logo
-fo.set(".Heineken", {rotation: 40});
-fo.to(".Heineken", {
-    duration: 1,
-    rotation:0,
-});
-
-// Specifications the coming in of bottle 1
-fo.from(".bottleOne", {
+// moves in the first bottle
+f1.from(".bottleOne", {
     duration: 1.5,
     opacity: 1,
     x: -800
     },0);
 
 //sets the first bottle up right
-fo.to(".bottleOne", {
+f1.to(".bottleOne", {
     duration: 1.5,
     rotation: 0,
     ease: "back"
 },0.25);
 
-// Specifications the coming in of bottle 2 including positioning
-fo.from(".bottleTwo", {
+// moves in the second bottle
+f1.from(".bottleTwo", {
     duration: 1.5,
     opacity: 1,
     x: -500
     },0);
 
 //sets the second bottle up right
-fo.to(".bottleTwo", {
+f1.to(".bottleTwo", {
     duration: 1.6,
     rotation: 0,
     ease: "back"
 },0.30);
 
-// Specifications the coming in of bottle 3 including positioning
-fo.from(".bottleThree", {
+// moves in the thirt bottle
+f1.from(".bottleThree", {
     duration: 1.5,
     opacity: 1,
     x: -300
     },0);
 
 //sets the third bottle up right
-fo.to(".bottleThree", {
+f1.to(".bottleThree", {
     duration: 1.7,
     rotation: 0,
     ease: "back"
 },0.30);
+
+// ==========================
+// initialisation and start of fase two
+// ==========================
+
+var f2 = gsap.timeline();
+
+f2.set(".bottleZeroOne", {scale:0});
+
+// ==========================
+// initialisation and start of fase three
+// ==========================
+
+var f3 = gsap.timeline();
+f3.set(".Heineken", {rotation: 20});
+
+// ******************************************
+//TODO: 
+//Start timeline after end of previous one
+// ******************************************
+
+//Move in the 0% and net zo lekker from the right with slight stagger
+f3.from(".Percentage, .Subheader", {
+    x:400,
+    stagger: 0.1
+},2);
+
+// Animating the heineken logo in
+f3.from(".Heineken", {
+    scale:0,
+    duration: 1,
+    ease: "back"
+});
+
+//Adjusting the rotation of the heinekenlogo halfway through
+f3.to(".Heineken",{
+    rotation:0,
+},"-=1");
